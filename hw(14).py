bd9afdf32c0450f80e3fb7fffd75d9db8c95e1f2
twkim <thomas.twoo.kim@gmail.com>
2024-11-19 21:25:32 +0300

    added hw15

--- hw(14).py
+++
@@ -1,6 +0,0 @@
-my_dict = {'x':500,'y':5874,'z':560}
-a = -100000000000000000
-for key,value in my_dict.items():
-    if value > a:
-        a = value
-print(f'maximum number:{a}')
--- hw(14-1).py
+++
@@ -1,9 +0,0 @@
-list_array = ['apple','banana', 'apple','orange']
-
-dic = {'apple':0, 'banana':0, 'orange':0}
-
-for list in list_array:
-    dic[list] += 1
-print(dic)
-
-
